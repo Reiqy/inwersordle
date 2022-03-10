fd82668a7726de69894dd049fac121501fac295b
Reiqy <kvetny.michal@gmail.com>
2022-03-10 23:50:57 +0100

    basic interface

--- inwersordle.js
+++ inwersordle.js
@@ -1,3 +1,12 @@
-let message = 'Hello world!';
-console.log(message);
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Yellow"] = 1] = "Yellow";
+    Color[Color["Green"] = 2] = "Green";
+})(Color || (Color = {}));
+class Game {
+    getRow(row) {
+        return new Array();
+    }
+}
 //# sourceMappingURL=inwersordle.js.map
--- inwersordle.ts
+++ inwersordle.ts
@@ -1,2 +1,19 @@
-let message: string = 'Hello world!';
-console.log(message);
+enum Color {
+    Gray,
+    Yellow,
+    Green,
+}
+
+interface Assignment {
+    getRow(row: number): Color[]
+}
+
+class Game implements Assignment {
+    getRow(row: number): Color[] {
+        return new Array<Color>();
+    }
+}
+
+interface GameProvider {
+    generateGame(): Game;
+}
