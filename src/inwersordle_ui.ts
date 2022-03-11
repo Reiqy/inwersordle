a851da47f0d7958ef001b9169a9dab1ac4b02117
Vojtech Kana <kana.vojtech@gmail.com>
2022-03-11 17:21:37 +0100

    Some basic grid input functionality added

--- src/inwersordle_ui.ts
+++ src/inwersordle_ui.ts
@@ -1,8 +1,8 @@
-function validate() {
-	return true;
-}
-
 function selectCell(selectedCell: HTMLElement) {
+	if (selectedCell.classList.contains("submitted")) {
+		return null;
+	}
+
 	let cells = document.getElementsByClassName("cell");
 	for (let cell of cells as any) {
 		cell.classList.remove("selected");
@@ -12,37 +12,87 @@ function selectCell(selectedCell: HTMLElement) {
 	return selectedCell;
 }
 
+function disableRow(selectedCell: HTMLElement) {
+	let rowCells = selectedCell.parentElement.querySelectorAll("div");
+	rowCells.forEach(function(cell: HTMLDivElement) {
+		cell.classList.add("submitted");
+	});
+}
+
+function selectNextCell(selectedCell: HTMLElement) {
+	// TODO
+}
+
+function selectPreviousCell(selectedCell: HTMLElement) {
+	// TODO
+}
+
+function checkCompleted() {
+	// TODO
+}
+
+function composeWord(selectedCell: HTMLElement) {
+	let s: string = "";
+	let children: NodeListOf<HTMLDivElement> = selectedCell.parentElement.querySelectorAll("div");
+	children.forEach(function(child: HTMLDivElement) {
+		s += child.innerHTML;
+	});
+
+	return s
+}
+
 $(function() {
 	let currentlySelected: HTMLElement = $(".row.first .cell.first")[0];
 	selectCell(currentlySelected);
 
 	$(".cell").on("click", function(e) {
-		currentlySelected = selectCell(e.target);
+		currentlySelected = selectCell(e.target) || currentlySelected;
 	});
 
 	$(document).on("keydown", function(e) {
-		if (e.key == "ArrowLeft") {
-			let previousSibling: HTMLElement = currentlySelected.previousElementSibling as HTMLElement;
-			if (previousSibling)
-				currentlySelected = selectCell(previousSibling);
-		} else if (e.key == "ArrowRight") {
-			let nextSibling: HTMLElement = currentlySelected.nextElementSibling as HTMLElement;
-			if (nextSibling)
-				currentlySelected = selectCell(nextSibling);
-		} else if (e.key.toString().length == 1 && (/[a-zA-Z]/.test(e.key))) {
+		if (currentlySelected == null) return;
+
+		switch (e.key) {
+			case "ArrowLeft":
+				selectPreviousCell(currentlySelected);
+				// TODO: refactor
+				let previousSibling: HTMLElement = currentlySelected.previousElementSibling as HTMLElement;
+				if (previousSibling) currentlySelected = selectCell(previousSibling) || currentlySelected;
+				break;
+			case "ArrowRight":
+				// TODO: refactor
+				selectNextCell(currentlySelected);
+				let nextSibling: HTMLElement = currentlySelected.nextElementSibling as HTMLElement;
+				if (nextSibling) currentlySelected = selectCell(nextSibling) || currentlySelected;
+				break;
+			case "Backspace":
+				currentlySelected.innerHTML = "";
+				selectPreviousCell(currentlySelected);
+				break;
+			case "Delete":
+				currentlySelected.innerHTML = "";
+				selectNextCell(currentlySelected);
+				break;
+		}
+
+		if (e.key.toString().length == 1 && (/[a-zA-Z]/.test(e.key))) {
 			currentlySelected.innerHTML = e.key;
 
+			checkCompleted();
+
+			// selectNextCell(currentlySelected);
 			if (currentlySelected.nextElementSibling)  {
-				currentlySelected = selectCell(currentlySelected.nextElementSibling as HTMLElement);
-				console.log(currentlySelected);
+				currentlySelected = selectCell(currentlySelected.nextElementSibling as HTMLElement) || currentlySelected;
 			} else {
-				if (validate()) {
-					console.log("validated -> next row!");
-					currentlySelected = selectCell(currentlySelected.parentElement.nextElementSibling.firstElementChild as HTMLElement);
+				disableRow(currentlySelected);
+
+				// TODO: Submit row
+				if (currentlySelected.parentElement.nextElementSibling != null) {
+					currentlySelected = selectCell(currentlySelected.parentElement.nextElementSibling.firstElementChild as HTMLElement) || currentlySelected;
+				} else {
+					currentlySelected = null;
 				}
 			}
 		}
 	});
-
-	console.log(currentlySelected);
 });
--- ts-built/inwersordle_ui.js
+++ ts-built/inwersordle_ui.js
@@ -1,7 +1,7 @@
-function validate() {
-    return true;
-}
 function selectCell(selectedCell) {
+    if (selectedCell.classList.contains("submitted")) {
+        return null;
+    }
     let cells = document.getElementsByClassName("cell");
     for (let cell of cells) {
         cell.classList.remove("selected");
@@ -9,37 +9,80 @@ function selectCell(selectedCell) {
     selectedCell.classList.add("selected");
     return selectedCell;
 }
+function disableRow(selectedCell) {
+    let rowCells = selectedCell.parentElement.querySelectorAll("div");
+    rowCells.forEach(function (cell) {
+        cell.classList.add("submitted");
+    });
+}
+function selectNextCell(selectedCell) {
+    // TODO
+}
+function selectPreviousCell(selectedCell) {
+    // TODO
+}
+function checkCompleted() {
+    // TODO
+}
+function composeWord(selectedCell) {
+    let s = "";
+    let children = selectedCell.parentElement.querySelectorAll("div");
+    children.forEach(function (child) {
+        s += child.innerHTML;
+    });
+    return s;
+}
 $(function () {
     let currentlySelected = $(".row.first .cell.first")[0];
     selectCell(currentlySelected);
     $(".cell").on("click", function (e) {
-        currentlySelected = selectCell(e.target);
+        currentlySelected = selectCell(e.target) || currentlySelected;
     });
     $(document).on("keydown", function (e) {
-        if (e.key == "ArrowLeft") {
-            let previousSibling = currentlySelected.previousElementSibling;
-            if (previousSibling)
-                currentlySelected = selectCell(previousSibling);
+        if (currentlySelected == null)
+            return;
+        switch (e.key) {
+            case "ArrowLeft":
+                selectPreviousCell(currentlySelected);
+                // TODO: refactor
+                let previousSibling = currentlySelected.previousElementSibling;
+                if (previousSibling)
+                    currentlySelected = selectCell(previousSibling) || currentlySelected;
+                break;
+            case "ArrowRight":
+                // TODO: refactor
+                selectNextCell(currentlySelected);
+                let nextSibling = currentlySelected.nextElementSibling;
+                if (nextSibling)
+                    currentlySelected = selectCell(nextSibling) || currentlySelected;
+                break;
+            case "Backspace":
+                currentlySelected.innerHTML = "";
+                selectPreviousCell(currentlySelected);
+                break;
+            case "Delete":
+                currentlySelected.innerHTML = "";
+                selectNextCell(currentlySelected);
+                break;
         }
-        else if (e.key == "ArrowRight") {
-            let nextSibling = currentlySelected.nextElementSibling;
-            if (nextSibling)
-                currentlySelected = selectCell(nextSibling);
-        }
-        else if (e.key.toString().length == 1 && (/[a-zA-Z]/.test(e.key))) {
+        if (e.key.toString().length == 1 && (/[a-zA-Z]/.test(e.key))) {
             currentlySelected.innerHTML = e.key;
+            checkCompleted();
+            // selectNextCell(currentlySelected);
             if (currentlySelected.nextElementSibling) {
-                currentlySelected = selectCell(currentlySelected.nextElementSibling);
-                console.log(currentlySelected);
+                currentlySelected = selectCell(currentlySelected.nextElementSibling) || currentlySelected;
             }
             else {
-                if (validate()) {
-                    console.log("validated -> next row!");
-                    currentlySelected = selectCell(currentlySelected.parentElement.nextElementSibling.firstElementChild);
+                disableRow(currentlySelected);
+                // TODO: Submit row
+                if (currentlySelected.parentElement.nextElementSibling != null) {
+                    currentlySelected = selectCell(currentlySelected.parentElement.nextElementSibling.firstElementChild) || currentlySelected;
+                }
+                else {
+                    currentlySelected = null;
                 }
             }
         }
     });
-    console.log(currentlySelected);
 });
 //# sourceMappingURL=inwersordle_ui.js.map
