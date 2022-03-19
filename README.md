11cd84b1ed1ce000cc0bd9de94bbdc1987a050d4
Vojtech Kana <kana.vojtech@gmail.com>
2022-03-20 00:31:26 +0100

    npx run format created + formatted code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # inwersordle
+
+## Instructions
+
+* `npm test` to run tests (located in `test/` folder)
+* `npm run build` to compile `src/*.ts` files to `ts-built/{}.js` files and create `main.js` using `Webpack`
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require("path");
 
 module.exports = {
-	entry: ['./ts-built/inwersordle.js', './ts-built/inwersordle_ui.js'],
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, '.'),
-	}
+  entry: ["./ts-built/inwersordle.js", "./ts-built/inwersordle_ui.js"],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "."),
+  },
 };
